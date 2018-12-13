07eeeafc3abddfc668f6103eac448530223eded8
Benjamin Dong-Hyun Lee <dhlee347@gmail.com>
2018-12-13 10:31:58 -0500

    modify open() and add explanation for input file format

--- README.md
+++ README.md
@@ -83,6 +83,21 @@ You should see 84%~88% accuracy in this task
 
 
 ### Pre-training Transformer
+Input file format :
+1. One sentence per line. These should ideally be actual sentences, not entire paragraphs or arbitrary spans of text. (Because we use the sentence boundaries for the "next sentence prediction" task).
+2. Blank lines between documents. Document boundaries are needed so that the "next sentence prediction" task doesn't span between documents.
+```
+Document 1 sentence 1
+Document 1 sentence 2
+...
+Document 1 sentence 45
+
+Document 2 sentence 1
+Document 2 sentence 2
+...
+Document 2 sentence 24
+```
+Usage :
 ```
 export DATA_FILE=/path/to/corpus
 export BERT_PRETRAIN=/path/to/pretrain
--- pretrain.py
+++ pretrain.py
@@ -30,6 +30,12 @@ import train
 
 from utils import set_seeds, get_device, get_random_word, truncate_tokens_pair
 
+# Input file format :
+# 1. One sentence per line. These should ideally be actual sentences,
+#    not entire paragraphs or arbitrary spans of text. (Because we use
+#    the sentence boundaries for the "next sentence prediction" task).
+# 2. Blank lines between documents. Document boundaries are needed
+#    so that the "next sentence prediction" task doesn't span between documents.
 
 def seek_random_offset(f, back_margin=2000):
     """ seek random offset of file pointer """
@@ -43,8 +49,8 @@ class SentPairDataLoader():
     """ Load sentence pair (sequential or random order) from corpus """
     def __init__(self, file, batch_size, tokenize, max_len, short_sampling_prob=0.1, pipeline=[]):
         super().__init__()
-        self.f_pos = open(file, 'r') # file pointer for a positive sample
-        self.f_neg = open(file, 'r') # file pointer for a negative (random) sample
+        self.f_pos = open(file, "r", encoding='utf-8', errors='ignore') # for a positive sample
+        self.f_neg = open(file, "r", encoding='utf-8', errors='ignore') # for a negative (random) sample
         self.tokenize = tokenize # tokenize function
         self.max_len = max_len # maximum length of tokens
         self.short_sampling_prob = short_sampling_prob
